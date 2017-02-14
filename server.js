a4e1cb4cffdb09b07157f3b3911d8c796d8a327b
Hongwei Peng <hongwei.peng@vodafone.com.au>
2017-02-14 15:13:05 +0800

    fix path

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'));
-
-// viewed at based directory http://localhost:8080/
 app.get('/', (req, res) => {
-  res.sendFile('/public/index.html');
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
